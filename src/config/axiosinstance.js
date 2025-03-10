7f6d155a7e3b83a1668adb6e5ff1ce1aaa72547c
Sandra p s <sandraps58038@gmail.com>
2025-03-10 22:32:13 +0530

    server-url-changed

--- src/config/axiosinstance.js
+++ src/config/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://projectbio-server.onrender.com',
  
   headers: {
     'Accept': 'application/json',
